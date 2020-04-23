a03f69c6c40bd029a7a44ad4b0726d50559565f0
varunraj <varunraj82@gmail.com>
2020-04-23 09:02:42 -0400

    added schema file

---
+++ schema/schema.js
@@ -0,0 +1,46 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql;
+
+const users = [
+    {id:'123', firstName: 'Bill', age: 23},
+    {id:'124', firstName: 'Andy', age: 33}
+];
+
+
+const UserType = new GraphQLObjectType({
+    name : 'User',
+    fields :{
+        id: {type:GraphQLString } ,
+        firstName: {type:GraphQLString } ,
+        age: {type:GraphQLInt } 
+
+    }
+});
+
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+
+        user:{
+            type:UserType,
+            args:{id:{type:GraphQLString}},
+            resolve(parentValue, args){
+                return _.find(users, {id:args.id});        
+            }    
+        }
+
+    }
+}) ;
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+});
+
